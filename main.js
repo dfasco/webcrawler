09fead9a9b357219928d3e25083635aca978fc75
dfasco <daniel.fasco84@gmail.com>
2023-05-20 20:40:33 -0400

    Import and call crawlPage function

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const { crawlPage } = require("./crawl");
+
 function main() {
   if (process.argv.length < 3) {
     console.log("no website provided");
@@ -9,6 +11,7 @@ function main() {
   }
   const baseURL = process.argv[2];
   console.log(`starting crawl of ${baseURL}`);
+  crawlPage(baseURL);
 }
 
 main();
